fbc064b4612d568e2d0d5d8e859c4209c633eef9
Ismael Fernando González Bort <ismael.gonzalez@externos-cl.cencosud.com>
2023-10-23 20:17:48 -0400

    add: JWT Validation Middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const { UserController } = require("../controllers");
+const { validateJwt } = require("../middlewares");
 
 const appRoute = Router();
 
 appRoute.post("/register", UserController.register);
 appRoute.post("/login", UserController.login);
-appRoute.get("/", UserController.getUsers);
+appRoute.get("/", [validateJwt], UserController.getUsers);
 
 module.exports = {
   appRoute,
